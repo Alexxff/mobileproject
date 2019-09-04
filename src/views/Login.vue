1afd3a42fa0831b626eb28728fc571035bb46f6e
Your Name <you@example.com>
2019-09-04 10:32:44 +0800

    登录功能-界面

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,19 @@
 <template>
-  <div></div>
+  <div>
+      <!-- 导航头 -->
+      <van-nav-bar title="登录" />
+
+          <!-- 输入框 -->
+          <van-cell-group>
+              <van-field left-icon="phone-o" placeholder="请输入手机号码"/>
+              <van-field left-icon="star-o" placeholder="请输入验证码">
+              <van-button slot="button" type="default" size="small">发送验证码</van-button></van-field>
+          </van-cell-group>
+          <!-- 登录按钮 -->
+          <div class="login-btn">
+            <van-button class="btn" type='info'>登录</van-button>
+          </div>
+  </div>
 </template>
 
 <script>
@@ -8,6 +22,11 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+.login-btn{
+  padding:20px;
+  .btn{
+    width:100%;
+  }
+}
 </style>
