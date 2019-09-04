f289b0e94d5bc75df9e0ae699815ba4654c29954
Your Name <you@example.com>
2019-09-04 21:49:13 +0800

    本地化（提示文字汉化）

--- src/main.js
+++ src/main.js
@@ -3,13 +3,18 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import Vant from 'vant'
-import VeeValidate from 'vee-validate'
+// import VeeValidate from 'vee-validate'
+import VeeValidate, { Validator } from 'vee-validate'
+// 导入VeeValidat的中文包
+import zhCN from 'vee-validate/dist/locale/zh_CN'
 import 'vant/lib/index.css'
 import './styles/index.less'
 
 Vue.use(Vant)
 // 配置插件VeeValidate
 Vue.use(VeeValidate)
+// 配置中文
+Validator.localize('zhCN', zhCN)
 Vue.config.productionTip = false
 new Vue({
   router,
