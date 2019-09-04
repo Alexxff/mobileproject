43400f35aaedf7146a1f728b4de0d26ea8374277
Your Name <you@example.com>
2019-09-04 14:52:16 +0800

    登录功能-封装发送登录的请求

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,10 +1,21 @@
 import axios from 'axios'
+import JSONbig from 'json-bigint'
 // 创建一个axios的实例,设置不同的baseURL
 const instance = axios.create({
-  timeout: 1000,
+  timeout: 5000,
   baseURL: ' http://ttapi.research.itcast.cn'
 })
-
+// 获取服务器返回的数据,并且是在处理数据之前使用
+// transformResponse 是在响应拦截器之前执行
+instance.defaults.transformResponse = [function (data) {
+  try {
+    return JSONbig.parse(data)
+  } catch (err) {
+  // 无法转换的数据直接原样返回
+    return data
+  }
+}
+]
 // Add a request interceptor
 instance.interceptors.request.use(function (config) {
 // Do something before request is sent
@@ -16,6 +27,7 @@ instance.interceptors.request.use(function (config) {
 
 // Add a response with interceptor
 instance.interceptors.response.use(function (response) {
+  console.log(response)
   // Do someting with resonse data
   return response
 }, function (error) {
